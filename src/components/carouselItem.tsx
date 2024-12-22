6072831112f82cded29d8b4a74cd4de8b7a3d0b8
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-12-21 22:53:42 -0800

    change drop shadow hover

--- src/components/carouselItem.tsx
+++ src/components/carouselItem.tsx
@@ -4,7 +4,7 @@ import { Album } from '@/app/api/v1/_data/data';
 
 const CarouselItem = ({ id, albumArtUrl, title, artist, releaseYear, genre }: Album) => {
   return (
-    <div key={id} className="carousel-item group hover:drop-shadow-2xl rounded-full  aspect-square carousel-center flex flex-col items-center p-40 bg-black cursor-pointer">
+    <div key={id} className="carousel-item group hover:drop-shadow-xl rounded-full carousel-center flex flex-col items-center p-40 bg-black cursor-pointer">
       <div className='grid grid-cols-1 justify-items-center items-center h-full'>
         <div className='col-start-1 row-start-1 bg-black opacity-55'>
           <Image
@@ -12,7 +12,7 @@ const CarouselItem = ({ id, albumArtUrl, title, artist, releaseYear, genre }: Al
             width={275}
             height={275}
             alt={title}
-            className="rounded-full z-0 group-hover:animate-[spin_3s_linear_infinite] min-w-full min-h-full transition object-contain object-center"
+            className="mask mask-circle z-0 group-hover:animate-[spin_3s_linear_infinite] min-w-full min-h-full transition object-contain object-center"
           />
         </div>
         <div className="mt-3 col-start-1 row-start-1 z-10 text-slate-300">
